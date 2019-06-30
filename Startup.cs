fefd1f76fc645e1db80f4afd80345e1192995fb5
Mikko Vuorinen <vuorinem@gmail.com>
2019-06-30 01:10:27 +0100

    Update to use .NET Core 2.2

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace WebApplicationBasic
         public void ConfigureServices(IServiceCollection services)
         {
           // Add framework services.
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
